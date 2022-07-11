363ccb540be3f3c9e90128e525d4f632204a61e0
vrinda <vrindajagtap@gmail.com>
2022-07-11 19:08:17 +0530

    First commit

--- src/main/java/com/demo/configuration/Configuration.java
+++ src/main/java/com/demo/configuration/Configuration.java
@@ -9,6 +9,6 @@ public class Configuration {
 	public static String browser="chrome";
 	public static String browserPath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\Drivers\\chromedriver.exe";
 	public static String email="vrindagunjal@gmail.com";
-	public static String password="eFcYr91mqCk4X4KV2EGIFEC4";
+	public static String password="ZzjCpwFG09NVjDD8Zh6S9E02";
 	public static String project="TF";
 }
