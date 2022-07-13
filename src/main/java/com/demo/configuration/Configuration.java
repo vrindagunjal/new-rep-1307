64ad577e72b78b40120218282a682df593978631
vrindagunjal <vrinda.gunjal@cogniwize.com>
2022-07-13 11:53:53 +0530

    First commit 1307

--- src/main/java/com/demo/configuration/Configuration.java
+++ src/main/java/com/demo/configuration/Configuration.java
@@ -1,14 +1,15 @@
 package com.demo.configuration;
 
 public class Configuration {
-	public static String urlJira="https://vbgunjal.atlassian.net/";
-	public static String host="vbgunjal.atlassian.net";
+	public static String urlJira="https://vrindagunjal.atlassian.net/";
+	public static String host="vrindagunjal.atlassian.net";
 	public static String url="https://opensource-demo.orangehrmlive.com";
-	public static String jsonCreatePath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\New Eclipse\\Projects\\SeleniumJiraJson\\createIssue.json";
-	public static String jsonEditPath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\New Eclipse\\Projects\\SeleniumJiraJson\\editIssue.json";
+	public static String jsonCreatePath="C:\\Users\\Vrinda Gunjal\\git\\repository7\\SeleniumJiraJson\\createIssue.json";
+	public static String jsonEditPath="C:\\Users\\Vrinda Gunjal\\git\\repository7\\SeleniumJiraJson\\editIssue.json";
 	public static String browser="chrome";
 	public static String browserPath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\Drivers\\chromedriver.exe";
-	public static String email="vrindagunjal@gmail.com";
-	public static String password="snw0H3gqistEh1yNQ8ZC7872";
-	public static String project="TF";
+	public static String email="vrinda.gunjal@cogniwize.com";
+	public static String password="kYjWcs0FBLBv0MMKVbRn9A5C";
+	public static String project="TEST";
 }
+	
